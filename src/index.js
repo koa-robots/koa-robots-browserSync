82eff49b9c9b4d30c963c3ec68156dbe2c9015d1
hanfeizi <g@ggg.gd>
2016-01-20 22:31:49 +0800

    fixed browser-sync-client.js 404 not found bug

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ export default function(roots = []){
 
     scripts = [
         '<script type="text/javascript" id="__bs_script__">',
-        'document.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.2.11.0.js\'><\/script>".replace("HOST", location.hostname));',
+        'document.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.2.11.1.js\'><\\/script>".replace("HOST", location.hostname));',
         '</script>',
         '</body>'
     ].join('')
