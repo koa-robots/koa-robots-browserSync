062e7431c5187b617b23a0292a95453eab284480
hanfeizi <g@ggg.gd>
2016-01-20 18:34:47 +0800

    remove CDATA

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,7 @@ exports.default = function () {
         return (0, _path.normalize)((0, _path.resolve)(root));
     });
 
-    scripts = ['<script type="text/javascript" id="__bs_script__">//<![CDATA[', 'document.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.2.11.0.js\'><\/script>".replace("HOST", location.hostname));', '//]]></script>', '</body>'].join('');
+    scripts = ['<script type="text/javascript" id="__bs_script__">', 'document.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.2.11.0.js\'><\/script>".replace("HOST", location.hostname));', '</script>', '</body>'].join('');
 
     _chokidar2.default.watch(roots, {
         ignored: /[\/\\]\./
--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ export default function(roots = []){
     })
 
     scripts = [
-        '<script type="text/javascript" id="__bs_script__">//<![CDATA[',
+        '<script type="text/javascript" id="__bs_script__">',
         'document.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.2.11.0.js\'><\/script>".replace("HOST", location.hostname));',
-        '//]]></script>',
+        '</script>',
         '</body>'
     ].join('')
 
